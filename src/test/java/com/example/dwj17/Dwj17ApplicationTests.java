6a8d5c55f7b9de336d9b01be21305be4c5713804
djeon <daewoong.jeon@kt.com>
2022-12-21 22:40:50 +0900

    first commit

---
+++ src/test/java/com/example/dwj17/Dwj17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dwj17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dwj17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
